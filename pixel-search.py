406055ad69d6d54ed9f3b633affc5411dae67170
TKselect <48096265+Takaaki37@users.noreply.github.com>
2024-02-28 15:51:34 +0900

    Update

--- pixel-search.py
+++ pixel-search.py
@@ -1,45 +1,39 @@
-'''
-画像のx, y座標を取得する
-実行すると画像が出るので、右クリックでconsoleに座標記載
-'''
-
-import urllib
+# OpenCVを使用して画像を表示し、右クリックした位置の座標を記録する
 import cv2
 
-#the [x, y] for each right-click event will be stored here
+# 右クリックイベントの座標を保存するリスト
 right_clicks = list()
 
-#this function will be called whenever the mouse is right-clicked
+# マウスが右クリックされた時に呼び出される関数
 def mouse_callback(event, x, y, flags, params):
-
-    #right-click event value is 2
-    if event == 2:
-        global right_clicks
-
-        #store the coordinates of the right-click event
+    # 右クリックイベントの値は2
+    if event == cv2.EVENT_RBUTTONDOWN:  # OpenCVのイベントを直接参照
+        # 座標をリストに追加
         right_clicks.append([x, y])
-
-        #this just verifies that the mouse data is being collected
-        #you probably want to remove this later
+        # マウスデータが収集されていることを確認（デバッグ用、最終的には削除推奨）
         print(right_clicks)
 
-#%%
+# 画像ファイルとパス
 imageFile = "image.jpg"
 path = "path"
 
-
-img = cv2.imread(path+"/"+imageFile)
+# 画像を読み込み
+img = cv2.imread(path + "/" + imageFile)
+# 画像のサイズを1000ピクセルに収まるように調整
 scale_width = 1000 / img.shape[1]
 scale_height = 1000 / img.shape[0]
 scale = min(scale_width, scale_height)
 window_width = int(img.shape[1] * scale)
 window_height = int(img.shape[0] * scale)
+
+# ウィンドウの設定（サイズ可変）
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('image', window_width, window_height)
 
-#set mouse callback function for window
+# マウスコールバック関数の設定
 cv2.setMouseCallback('image', mouse_callback)
 
+# 画像の表示
 cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.waitKey(0)  # 何かキーが押されるまで待機
+cv2.destroyAllWindows()  # すべてのウィンドウを閉じる
--- video-image.py
+++ video-image.py
@@ -1,32 +1,42 @@
+
+# 指定されたビデオファイルからフレームを抽出して保存する
+
 import cv2
 import os
 
+def save_frame_range(video_path, start_frame, stop_frame, step_frame, dir_path, basename, ext='jpg'):
+    """
+    ビデオファイルから指定された範囲のフレームを抽出して保存する関数。
+    
+    Parameters:
+    - video_path: ビデオファイルのパス
+    - start_frame: 開始フレーム番号
+    - stop_frame: 終了フレーム番号
+    - step_frame: フレームを抽出する間隔
+    - dir_path: 保存するディレクトリのパス
+    - basename: 保存するファイルのベース名
+    - ext: 保存するファイルの拡張子（デフォルトは'jpg'）
+    """
+    cap = cv2.VideoCapture(video_path)  # ビデオを読み込み
 
-#%%
-def save_frame_range(video_path, start_frame, stop_frame, step_frame,
-                     dir_path, basename, ext='jpg'):
-    cap = cv2.VideoCapture(video_path)
-
-    if not cap.isOpened():
+    if not cap.isOpened():  # ビデオが開けなかった場合は終了
         return
 
-    os.makedirs(dir_path, exist_ok=True)
-    base_path = os.path.join(dir_path, basename)
+    os.makedirs(dir_path, exist_ok=True)  # 保存ディレクトリを作成（既に存在する場合は作成しない）
+    base_path = os.path.join(dir_path, basename)  # 保存するファイルの基本パス
 
-    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))  # フレーム数に合わせたゼロ埋めの桁数を計算
 
-    for n in range(start_frame, stop_frame, step_frame):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, n)
-        ret, frame = cap.read()
-        if ret:
-            cv2.imwrite('{}_{}.{}'.format(base_path, str(n).zfill(digit), ext), frame)
-        else:
+    for n in range(start_frame, stop_frame, step_frame):  # 指定された範囲とステップでループ
+        cap.set(cv2.CAP_PROP_POS_FRAMES, n)  # 抽出するフレーム位置を設定
+        ret, frame = cap.read()  # フレームを読み込み
+        if ret:  # フレームが正常に読み込まれた場合
+            # ファイル名にフレーム番号を付けて保存
+            cv2.imwrite(f'{base_path}_{str(n).zfill(digit)}.{ext}', frame)
+        else:  # フレームの読み込みに失敗した場合は終了
             return
 
+# ビデオファイルからフレームを抽出して保存する関数の呼び出し例
+save_frame_range('MOV.mp4', 0, 10000, 1, 'result2', 'frame')
 
-
-save_frame_range('MOV.mp4',
-                 0, 10000, 1,
-                 'result2', 'frame')
 
